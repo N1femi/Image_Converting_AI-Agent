865e0debf06513b1a1c02d2b4ce6cd92cc05d2b6
N1femi <domolitice@gmail.com>
2025-06-18 07:37:05 -0400

    Revert "Packaged Application Within Electron"
    This reverts commit a1a9a2fb0ec788c990247ee907a791384f22d7ec.

--- renderer.js
+++
@@ -1,4 +0,0 @@
-// Probably make sure file is valid for the type of Converion(Images)
-// When user chooses file display image in preview
-// Once user clicks convert, take them to the converter page
-// User can cancel conversion process anytime
